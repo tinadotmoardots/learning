192abebf4ae58dbe6fa7feb3b13a9f96e3e6235d
tinadotmoardots <79804336+tinadotmoardots@users.noreply.github.com>
2021-02-28 13:51:45 -0700

    Update randomnumbers.sh

--- randomnumbers.sh
+++ randomnumbers.sh
@@ -1,3 +1,5 @@
   
 #!/bin/bash
 $ echo $(( $RANDOM % 10 + 1 ))
+# This script will display a number from 1-10. 
+# Writing this taught me the importance of capital letters sarah if you don't use them the codes like no u
