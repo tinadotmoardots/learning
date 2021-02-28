c5833ef34f36326724e812d8890552d11a91d405
tinadotmoardots <79804336+tinadotmoardots@users.noreply.github.com>
2021-02-28 13:53:58 -0700

    Update randomnumbers.sh

--- randomnumbers.sh
+++ randomnumbers.sh
@@ -1,5 +1,5 @@
   
 #!/bin/bash
 $ echo $(( $RANDOM % 10 + 1 ))
-# This script will display a number from 1-10. 
+# This script will display a number from 1-10. Echo is the part that prints it, RANDOM chooses a random number, the % 10 +1 limits it to 1-10
 # Writing this taught me the importance of capital letters sarah if you don't use them the codes like no u
