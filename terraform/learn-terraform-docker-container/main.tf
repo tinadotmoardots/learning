e1cd045d43f68e9092ca4ed669f3c791ac085789
Kristina Hunter <79804336+tinadotmoardots@users.noreply.github.com>
2021-09-08 18:19:21 -0700

    terraform learning
    Various Hashicorp tutorials

---
+++ terraform/learn-terraform-docker-container/main.tf
@@ -0,0 +1,27 @@
+#This code provisions an NGINX webserver with terraform via docker, copied from https://learn.hashicorp.com/tutorials/terraform/install-cli?in=terraform/certification-associate-tutorials
+
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
+
